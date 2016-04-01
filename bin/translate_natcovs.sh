67aa472c2ac6de51d2aa6949155ec99a76032ce2
Alex Ip <alex@trentham.net.au>
2016-04-01 17:10:27 +1100

    Typos fixed

--- bin/translate_natcovs.sh
+++ bin/translate_natcovs.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
+script_dir=$(readlink -f ${0%/*})
 for zip in $(find /g/data1/rr2/National_Coverages/ -name "*.zip" | sort)
 do
   echo Translating file $zip to NetCDF
-  ./geophys2netcdf.sh $zip
+  $script_dir/geophys2netcdf.sh $zip
 done
--- bin/update_natcovs.sh
+++ bin/update_natcovs.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
+script_dir=$(readlink -f ${0%/*})
 for nc in $(find /g/data1/rr2/National_Coverages/ -name "*.nc" | sort)
 do
   echo Updating NetCDF Metadata for file $nc
-  ./geophys2netcdf.sh $nc
+  $script_dir/geophys2netcdf.sh $nc
 done
