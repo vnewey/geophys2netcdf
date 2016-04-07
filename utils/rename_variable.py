0e167578275ab80db3cba9db37f7689bc20c26a4
Alex Ip <alex@trentham.net.au>
2016-04-07 15:33:51 +1000

    Robustness improved

--- utils/rename_variable.py
+++ utils/rename_variable.py
@@ -14,7 +14,7 @@ def main():
     assert len(sys.argv) in [4,5], 'Usage: %s <root_dir> <file_template> <new_variable_name> [<long_variable_name>]' % sys.argv[0]
     root_dir = sys.argv[1]
     file_template = sys.argv[2]
-    new_variable_name = sys.argv[3]
+    new_variable_name = re.sub('\W', '_', sys.argv[3]) # Sanitise new name
     
     if len(sys.argv) == 5:
         long_variable_name = sys.argv[4]
@@ -32,7 +32,7 @@ def main():
         old_variable_name = [key for key in nc_dataset.variables.keys() if key not in ['crs', 'lat', 'lon']][0]
     
         if new_variable_name != old_variable_name:
-            nc_dataset.renameVariable(old_variable_name, re.sub('\W', '_', new_variable_name))
+            nc_dataset.renameVariable(old_variable_name, new_variable_name)
             print '%s.variables["%s"] renamed to %s' % (nc_path, old_variable_name, new_variable_name)
         
         variable = nc_dataset.variables[new_variable_name]
@@ -41,7 +41,7 @@ def main():
             variable.long_name = long_variable_name
             print '%s.variables["%s"].long_name changed to %s' % (nc_path, new_variable_name, long_variable_name)
 
-        # Retrospective fixup
+        # Retrospective fixupZZ
         nc_dataset.Conventions = nc_dataset.Conventions.replace('CF-1.5', 'CF-1.6').replace(', ', ',')
     
         nc_dataset.close()
