9c356818aa7e40ccb2e2c2a9fe4fdf000cc42788
Alex Ip <alex@trentham.net.au>
2016-08-15 18:50:03 +1000

    Debugging data_stats.py. Still not working

--- hacks/data_stats.py
+++ hacks/data_stats.py
@@ -13,7 +13,7 @@ class DataStats(object):
     classdocs
     '''
     key_list = ['nc_path', 'nodata_value', 'min', 'max', 'mean', 'median', 'percentile_1', 'percentile_99']
-    CHUNK_MULTIPLE = 4 # How many chunks to grab at a time
+    CHUNK_MULTIPLE = 32 # How many chunks to grab at a time
 
     def __init__(self, netcdf_path):
         '''
@@ -32,6 +32,7 @@ class DataStats(object):
         self._data_stats['nodata_value'] = data_variable._FillValue        
 
         try:
+            raise Exception('Testing only')
             data_array = variable[:].data # This will fail for larger than memory arrays
             data_array = data_array[data_array != self._data_stats['nodata_value']] # Discard all no-data elements
             netcdf_dataset.close()
@@ -44,8 +45,10 @@ class DataStats(object):
             self._data_stats['median'] = np.nanpercentile(data_array, 50)
             self._data_stats['percentile_1'] = np.nanpercentile(data_array, 1)
             self._data_stats['percentile_99'] = np.nanpercentile(data_array, 99)
-        except:
-            shape = data_variable.shape()
+        except Exception, e:
+            shape = list(reversed(data_variable.shape))
+
+            print 'Whole-array read failed (%s) for array size %s' % (e.message, shape)
             chunking = [DataStats.CHUNK_MULTIPLE * chunk_size for chunk_size in data_variable.chunking()]
             start_indices = [0,0]
             end_indices = [0,0]
@@ -59,16 +62,32 @@ class DataStats(object):
                 start_indices[1] = 0
                 for _dimension1_index in range(chunk_counts[1]):
                     end_indices[1] = min(start_indices[1] + chunking[1], shape[1])
+                    print 'Range = %d:%d, %d:%d' % (start_indices[0], end_indices[0], start_indices[1], end_indices[1])
                     chunk_array = variable[start_indices[0]:end_indices[0], start_indices[1]:end_indices[1]].data
-                    chunk_length = len(chunk_array)
+                    print chunk_array.shape
                     chunk_array = chunk_array[chunk_array != self._data_stats['nodata_value']] # Discard all no-data elements
-                    
-                    self._data_stats['min'] = min(self._data_stats['min'], np.nanmin(chunk_array))
-                    self._data_stats['max'] = max(self._data_stats['max'], np.nanmax(chunk_array))
-                    weighted_mean = weighted_mean + np.nanmean(data_array) * chunk_length
-                    length_read += chunk_length
-                    
+                    chunk_length = len(chunk_array)
+
+                    if chunk_length:
+                        try:
+                            self._data_stats['min'] = min(self._data_stats['min'], np.nanmin(chunk_array))
+                        except:
+                            self._data_stats['min'] = np.nanmin(chunk_array)
+
+                        try:
+                            self._data_stats['max'] = max(self._data_stats['max'], np.nanmax(chunk_array))
+                        except:
+                            self._data_stats['max'] = np.nanmax(chunk_array)
+
+                        weighted_mean = weighted_mean + np.nanmean(chunk_array) * chunk_length
+                        length_read += chunk_length
+                    else:
+                        print 'Empty array'
+
+                    del chunk_array
+                    gc.collect()
                     start_indices[1] = end_indices[1]
+
                 start_indices[0] = end_indices[0]
             
             self._data_stats['mean'] = weighted_mean / length_read
