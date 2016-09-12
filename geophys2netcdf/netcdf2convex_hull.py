f9e4f64622d8dea46fcd0ac66aa04af2af6dd793
Alex Ip <alex@trentham.net.au>
2016-09-12 17:09:51 +1000

    Switched from using scipy.spatial to shapely to gain access to polygon buffering functions.

--- geophys2netcdf/netcdf2convex_hull.py
+++ geophys2netcdf/netcdf2convex_hull.py
@@ -7,14 +7,18 @@ import numpy as np
 #import netCDF4
 import gc
 from scipy import ndimage
-from scipy.spatial import ConvexHull
+import shapely.geometry as geometry
+from shapely.ops import polygonize
 from geophys2netcdf.array_pieces import array_pieces
 
 def netcdf2convex_hull(netcdf_dataset, max_bytes=None):
     '''
-    Function to return a numpy array containing coordinates of the vertices of a closed convex hull around data-containing areas
+    Function to return a list of vertices in the convex hull around data-containing areas
     '''
     def get_edge_points(netcdf_dataset, max_bytes=None):
+        '''
+        Function to return a list of points corresponding to pixels on the edge of data-containing areas
+        '''
         # Find variable with "grid_mapping" attribute - assumed to be 2D data variable
         try:
             data_variable = [variable for variable in netcdf_dataset.variables.values() if hasattr(variable, 'grid_mapping')][0]
@@ -26,7 +30,7 @@ def netcdf2convex_hull(netcdf_dataset, max_bytes=None):
         dimension_variable = [netcdf_dataset.variables[data_variable.dimensions[dim_index]] for dim_index in range(2)]
         nodata_value = data_variable._FillValue
         
-        piece_point_list = [] # List of point arrays
+        point_list = [] # Complete list of edge points (unknown length)
         for piece_array, array_offset in array_pieces(data_variable):
             dimension_subset = [dimension_variable[dim_index][array_offset[dim_index]:piece_array.shape[dim_index]] for dim_index in range(2)]
             
@@ -41,41 +45,33 @@ def netcdf2convex_hull(netcdf_dataset, max_bytes=None):
     
             if edges[0].size:
                 piece_points = np.zeros((edges[0].size, 2), dtype=netcdf_dataset.variables[data_variable.dimensions[0]].dtype)
+                #TODO: Do something more general here to account for YX or XY dimension order
                 piece_points[:,1] = dimension_subset[0][edges[0]]
                 piece_points[:,0] = dimension_subset[1][edges[1]]
-                piece_point_list.append(piece_points)
+                point_list += list(piece_points)
 #                print '%s edge points found' % piece_points.shape[0]
 #            else:
 #                print 'No edge points found'
 
             del piece_array
+            del piece_points
             gc.collect()
             
-        # Determine total number of points and create points array
-        point_count = 0
-        for piece_points in piece_point_list:
-            point_count += piece_points.shape[0]
-            
-        points = np.zeros((point_count, 2), dtype=netcdf_dataset.variables[data_variable.dimensions[0]].dtype)
+        return point_list
         
-        point_count = 0
-        for piece_points in piece_point_list:
-            start_index = point_count
-            point_count += piece_points.shape[0]
-            points[start_index:point_count,:] = piece_points
-        
-        return points
-        
-    # Start of netCDF2convex_hull function
-    points = get_edge_points(netcdf_dataset, max_bytes)
+    # Start of netcdf2convex_hull function
+    GeoTransform = [float(number) for number in netcdf_dataset.variables['crs'].GeoTransform.strip().split(' ')]
+    avg_pixel_size = (abs(GeoTransform[1]) + abs(GeoTransform[5])) / 2.0
+
+    convex_hull = geometry.MultiPoint(get_edge_points(netcdf_dataset, max_bytes)).convex_hull
     
-    convex_hull = ConvexHull(points)
-    vertex_count = len(convex_hull.vertices)
+    convex_hull = convex_hull.buffer(avg_pixel_size/2.0, cap_style=2, join_style=2, mitre_limit=avg_pixel_size).simplify(avg_pixel_size) # Offset outward by half a pixel width    
     
-    vertices = np.zeros((vertex_count + 1, 2), dtype=points.dtype) # Allow for repeated start/end point
+    #===========================================================================
+    # # Perform offsets to (hopefully) simplify shape and take pixel size into account
+    # convex_hull = convex_hull.buffer(-avg_pixel_size / 2.0, cap_style=3, join_style=3, mitre_limit=avg_pixel_size) # Offset inward by half a pixel width
+    # convex_hull = convex_hull.buffer(avg_pixel_size, cap_style=3, join_style=3, mitre_limit=avg_pixel_size) # Offset outward by a whole pixel width    
+    #===========================================================================
     
-    vertices[0:vertex_count] = points[convex_hull.vertices]
-    vertices[-1] = vertices[0] # Close shape by repeating first vertex
-    
-    return vertices
+    return [coordinates for coordinates in convex_hull.exterior.coords] # Convert generator to list
     
