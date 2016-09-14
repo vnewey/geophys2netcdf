de428c23d70e1071ffa6c889b78886b86a63a9d7
Alex Ip <alex@trentham.net.au>
2016-09-14 10:24:48 +1000

    Code refactored to expose the points2convex_hull function

--- geophys2netcdf/netcdf2convex_hull.py
+++ geophys2netcdf/netcdf2convex_hull.py
@@ -1,18 +1,19 @@
 '''
+Functions to obtain edge points and convex hull vertices from a gridded NetCDF dataset
+
 Created on 12Sep.,2016
 
 @author: Alex Ip
 '''
 import numpy as np
 #import netCDF4
-import gc
 from scipy import ndimage
 import shapely.geometry as geometry
 from geophys2netcdf.array_pieces import array_pieces
 
 def get_edge_points(netcdf_dataset, max_bytes=None):
     '''
-    Function to return a list of points corresponding to pixels on the edge of data-containing areas
+    Function to return a list of coordinates corresponding to pixels on the edge of data-containing areas of the NetCDF dataset
     '''
     # Find variable with "grid_mapping" attribute - assumed to be 2D data variable
     try:
@@ -25,34 +26,56 @@ def get_edge_points(netcdf_dataset, max_bytes=None):
     dimension_variable = [netcdf_dataset.variables[data_variable.dimensions[dim_index]] for dim_index in range(2)]
     nodata_value = data_variable._FillValue
     
-    point_list = [] # Complete list of edge points (unknown length)
+    edge_point_list = [] # Complete list of edge points (unknown length)
     for piece_array, array_offset in array_pieces(data_variable, max_bytes):
         dimension_subset = [dimension_variable[dim_index][array_offset[dim_index]:array_offset[dim_index]+piece_array.shape[dim_index]] for dim_index in range(2)]
         
+        # Convert masked array to plain array
         if type(piece_array) == np.ma.core.MaskedArray:
             piece_array = piece_array.data
 #        print 'array_offset = %s, piece_array.shape = %s, piece_array.size = %s' % (array_offset, piece_array.shape, piece_array.size)
-        piece_array = (piece_array != nodata_value)
+
+        piece_array = (piece_array != nodata_value) # Convert to boolean True=data/False=no-data array
         
         # Detect edges
-        edges = np.where(ndimage.filters.maximum_filter(piece_array, size=2) != 
+        edge_ordinates = np.where(ndimage.filters.maximum_filter(piece_array, size=2) != 
                          ndimage.filters.minimum_filter(piece_array, size=2))
 
-        if edges[0].size:
-            piece_points = np.zeros((edges[0].size, 2), dtype=netcdf_dataset.variables[data_variable.dimensions[0]].dtype)
+        if edge_ordinates[0].size:
+            piece_edge_points = np.zeros((edge_ordinates[0].size, 2), dtype=netcdf_dataset.variables[data_variable.dimensions[0]].dtype)
             #TODO: Do something more general here to account for YX or XY dimension order
-            piece_points[:,1] = dimension_subset[0][edges[0]]
-            piece_points[:,0] = dimension_subset[1][edges[1]]
-            point_list += list(piece_points)
-#            print '%s edge points found' % piece_points.shape[0]
+            piece_edge_points[:,1] = dimension_subset[0][edge_ordinates[0]]
+            piece_edge_points[:,0] = dimension_subset[1][edge_ordinates[1]]
+            edge_point_list += list(piece_edge_points)
+#            print '%s edge points found' % piece_edge_points.shape[0]
 #        else:
 #            print 'No edge points found'
 
-    return point_list
-        
+    return edge_point_list
+
+
+def points2convex_hull(point_list, dilation=0, tolerance=0):
+    '''
+    Function to return a list of vertex coordinates in the convex hull around data-containing areas of a point list
+    Parameters:
+        point_list: Iterable containing points from which to compute convex hull
+        dilation: distance to dilate convex hull
+        tolerance: distance tolerance for the simplification of the convex hull
+    '''
+    convex_hull = geometry.MultiPoint(point_list).convex_hull
+    
+    # Offset outward by a full pixel width (instead of half) and simplify with one pixel width tolerance
+    convex_hull = convex_hull.buffer(dilation, cap_style=2, join_style=2, mitre_limit=tolerance).simplify(tolerance)
+    
+    return [coordinates for coordinates in convex_hull.exterior.coords] # Convert generator to list
+    
+    
 def netcdf2convex_hull(netcdf_dataset, max_bytes=None):
     '''
-    Function to return a list of vertices in the convex hull around data-containing areas
+    Function to return a list of vertex coordinates in the convex hull around data-containing areas of the NetCDF dataset
+    Parameters:
+        netcdf_dataset: netCDF4.Dataset object
+        max_bytes: Maximum number of bytes to retrieve in each array piece
     '''
     # Find variable with "GeoTransform" attribute - assumed to be grid mapping variable
     try:
@@ -61,11 +84,6 @@ def netcdf2convex_hull(netcdf_dataset, max_bytes=None):
         raise Exception('Unable to determine grid mapping variable (must have "GeoTransform" attribute')
     GeoTransform = [float(number) for number in grid_mapping_variable.GeoTransform.strip().split(' ')]
     avg_pixel_size = (abs(GeoTransform[1]) + abs(GeoTransform[5])) / 2.0
-
-    convex_hull = geometry.MultiPoint(get_edge_points(netcdf_dataset, max_bytes)).convex_hull
-    
-    # Offset outward by a full pixel width (instead of half) and simplify   
-    convex_hull = convex_hull.buffer(avg_pixel_size, cap_style=2, join_style=2, mitre_limit=avg_pixel_size).simplify(avg_pixel_size)
-    
-    return [coordinates for coordinates in convex_hull.exterior.coords] # Convert generator to list
+   
+    return points2convex_hull(get_edge_points(netcdf_dataset, max_bytes), avg_pixel_size, avg_pixel_size) 
     
