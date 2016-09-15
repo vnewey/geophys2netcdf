7041e43e4a81b2e1f42202edfe33f864f3963be1
Alex Ip <alex@trentham.net.au>
2016-09-15 18:15:21 +1000

    Vectorised get_value_at_coords function but not happy with current method: To be continued.

--- geophys2netcdf/netcdf_2d_utils.py
+++ geophys2netcdf/netcdf_2d_utils.py
@@ -15,17 +15,15 @@ class NetCDF2DUtils(object):
     '''
     # Assume WGS84 lat/lon if no CRS is provided
     DEFAULT_CRS = "GEOGCS[\"WGS 84\",DATUM[\"WGS_1984\",SPHEROID[\"WGS 84\",6378137,298.257223563,AUTHORITY[\"EPSG\",\"7030\"]],AUTHORITY[\"EPSG\",\"6326\"]],PRIMEM[\"Greenwich\",0,AUTHORITY[\"EPSG\",\"8901\"]],UNIT[\"degree\",0.0174532925199433,AUTHORITY[\"EPSG\",\"9122\"]],AUTHORITY[\"EPSG\",\"4326\"]]"
-
+    HORIZONTAL_VARIABLE_NAMES = ['lon', 'Easting', 'x', 'longitude']
+    
     def __init__(self, netcdf_dataset):
         '''
         NetCDF2DUtils Constructor
         '''
         self.netcdf_dataset = netcdf_dataset
         
-        assert len(self.netcdf_dataset.dimensions) == 2, 'NetCDF dataset must be 2D'
-        
-        # Two-element list of dimension varibles. Assumed to be ordered XY
-        self.dimension_arrays = [self.netcdf_dataset.variables[dimension_name][:] for dimension_name in self.netcdf_dataset.dimensions.keys()]
+#        assert len(self.netcdf_dataset.dimensions) == 2, 'NetCDF dataset must be 2D' # This is not valid
         
         # Find variable with "grid_mapping" attribute - assumed to be 2D data variable
         try:
@@ -33,18 +31,23 @@ class NetCDF2DUtils(object):
         except:
             raise Exception('Unable to determine data variable (must have "grid_mapping" attribute')
         
-        # Set up dict to account for YX array ordering
-#        self.array_dim = {dim_index: self.data_variable.dimensions.index(self.netcdf_dataset.dimensions.keys()[dim_index]) for dim_index in range(2)}
-
-        self.YX_order = (self.data_variable.dimensions != self.netcdf_dataset.dimensions.keys())
+        # Boolean flag indicating YX array ordering
+        #TODO: Find a nicer way of dealing with this
+        self.YX_order = self.data_variable.dimensions[1] in  NetCDF2DUtils.HORIZONTAL_VARIABLE_NAMES
+        
+        # Two-element list of dimension varibles. 
+        self.dimension_arrays = [self.netcdf_dataset.variables[dimension_name][:] for dimension_name in self.data_variable.dimensions]
         
         self.grid_mapping_variable = netcdf_dataset.variables[self.data_variable.grid_mapping]
         
         self.GeoTransform = [float(number) for number in self.grid_mapping_variable.GeoTransform.strip().split(' ')]
         
-        self.pixel_size = (abs(self.GeoTransform[1]), abs(self.GeoTransform[5]))
-        self.min_extent = tuple([min(self.dimension_arrays[dim_index]) - self.pixel_size[dim_index] for dim_index in range(2)])
-        self.max_extent = tuple([max(self.dimension_arrays[dim_index]) + self.pixel_size[dim_index] for dim_index in range(2)])
+        self.pixel_size = [abs(self.GeoTransform[1]), abs(self.GeoTransform[5])]
+        if self.YX_order:
+            self.pixel_size.reverse()
+        
+        self.min_extent = tuple([min(self.dimension_arrays[dim_index]) - self.pixel_size[dim_index]/2.0 for dim_index in range(2)])
+        self.max_extent = tuple([max(self.dimension_arrays[dim_index]) + self.pixel_size[dim_index]/2.0 for dim_index in range(2)])
         
     
     def get_coordinate_transformation(self, crs=None, to_native=True):
@@ -73,11 +76,11 @@ class NetCDF2DUtils(object):
         
     
         
-    def native_coords(self, coordinates, crs=None):
+    def get_native_coords(self, coordinates, crs=None):
         coord_trans = self.get_coordinate_transformation(crs, to_native=True)
         
         if not coord_trans:
-            return coordinates
+            return list(coordinates)
         
         try: # Multiple coordinates
             return [coordinate[0:2] for coordinate in coord_trans.TransformPoints(coordinates)]
@@ -85,33 +88,35 @@ class NetCDF2DUtils(object):
             return coord_trans.TransformPoint(*coordinates)[0:2]
         
     
-    def indices_from_coord(self, coordinate, crs=None):
+    def get_indices_from_coords(self, coordinates, crs=None):
         '''
         '''
-        native_coordinates = self.native_coords(coordinate, crs)
+        native_coordinates = self.get_native_coords(coordinates, crs)
         
+        # Convert coordinates to same order as array
+        if self.YX_order:
+            try:
+                for coord_index in range(len(native_coordinates)):
+                    if native_coordinates[coord_index] is not None:
+                        native_coordinates[coord_index] = list(native_coordinates[coord_index])
+                        native_coordinates[coord_index].reverse()
+            except:
+                native_coordinates = list(native_coordinates)            
+                native_coordinates.reverse()
         try: # Multiple coordinates
             indices = [[np.where(abs(self.dimension_arrays[dim_index] - coordinate[dim_index]) <= (self.pixel_size[dim_index] / 2.0))[0][0] for dim_index in range(2)]
                        if not ([True for dim_index in range(2) if coordinate[dim_index] < self.min_extent[dim_index] or coordinate[dim_index] > self.max_extent[dim_index]])
                        else None
                        for coordinate in native_coordinates]
-            
-            if self.YX_order:
-                for index_pair in indices:
-                    if index_pair is not None:
-                        index_pair.reverse()
-        except: # Singlue coordinate pair
+        except: # Single coordinate pair
             indices = ([np.where(abs(self.dimension_arrays[dim_index] - native_coordinates[dim_index]) <= (self.pixel_size[dim_index] / 2.0))[0][0] for dim_index in range(2)]
                        if not [True for dim_index in range(2) if native_coordinates[dim_index] < self.min_extent[dim_index] or native_coordinates[dim_index] > self.max_extent[dim_index]]
                        else None)
-    
-            if self.YX_order and indices:
-                indices.reverse()
             
         return indices
         
         
-    def value_at_coord(self, coordinates, crs=None, variable_name=None):
+    def get_value_at_coords(self, coordinates, crs=None, variable_name=None):
         '''
         '''
         if variable_name:
@@ -119,13 +124,26 @@ class NetCDF2DUtils(object):
         else:
             data_variable = self.data_variable
             
-        indices = self.indices_from_coord(coordinates, crs)
+        no_data_value = data_variable._FillValue
+
+        indices = self.get_indices_from_coords(coordinates, crs)
         
         if not indices:
             return None
-        
+                
         try:
-            return list([data_variable[index_pair[0], index_pair[1]] if index_pair else None for index_pair in indices])
+            # Make this a vectorised operation for speed (one query for all points)
+            mask_array = np.array([(index_pair is not None) for index_pair in indices]) # Boolean mask indicating which index pairs are valid
+            index_array = np.array([index_pair for index_pair in indices if index_pair is not None]) # Array of valid index pairs only
+        
+            # N.B: ".diagonal()" is required because NetCDF doesn't do advanced indexing exactly like numpy
+            # Ugly hack is required to take values from leading diagonal. Requires n^2 elements retrieved instead of n. Not good
+            #TODO: Think of a better way of doing this
+            value_array = data_variable[(index_array[:,0], index_array[:,1])].diagonal()
+            print value_array
+            result_array = np.ones(shape=(len(indices)), dtype=value_array.dtype) * no_data_value
+            result_array[mask_array] = value_array
+            return list(result_array)
         except:
             return data_variable[indices[0], indices[1]]
         
