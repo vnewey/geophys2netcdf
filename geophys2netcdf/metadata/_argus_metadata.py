7587b35d91327b042d29b0c7cf22d7e4404ea9c5
Alex Ip <alex@trentham.net.au>
2017-01-13 14:31:09 +1100

    Argus and JetCat metadata readers created

--- geophys2netcdf/metadata/_argus_metadata.py
+++ geophys2netcdf/metadata/_argus_metadata.py
@@ -9,21 +9,28 @@ Written: 5/7/2016
 
 import logging
 import cx_Oracle
+import os
+import re
 import sys
 from pprint import pprint
 
 from geophys2netcdf.metadata import Metadata
 
 logger = logging.getLogger('root.' + __name__)
-
+logger.setLevel(logging.INFO)  # Initial logging level for this module
 
 class ArgusMetadata(Metadata):
     """Subclass of Metadata to manage Argus data
     """
     
+        # Class variable holding metadata type string
+    _metadata_type_id = 'Argus'
+    _filename_pattern = '.*'  # Default RegEx for finding metadata file.
+
     # Murray's Argus query
+    # N.B: Need to use text substitution for multiple survey IDs - can't use parameterised query
     ARGUS_QUERY = '''SELECT
-       TO_NUMBER(regexp_substr(A.SURVEYS.SURVEYID, '^\d+$')),
+       TO_NUMBER(regexp_substr(A.SURVEYS.SURVEYID, '^\d+$')) as SURVEYID,
        A.SURVEYS.SURVEYNAME,
        A.SURVEYS.STATE,
        A.SURVEYS.OPERATOR,
@@ -67,14 +74,29 @@ join argus.airsurveys using (eno)
 left join argus.airdem using (eno)
 left join argus.airmag using (eno)
 left join argus.airrad using (eno)
---left join argus.survey_remarks on A.SURVEYS.SURVEYID = argus.survey_remarks.SURVEYID
-where TO_NUMBER(regexp_substr(A.SURVEYS.SURVEYID, '^\d+$')) = :SURVEYID
---order by TO_NUMBER(A.SURVEYS.SURVEYID)'''
+where TO_NUMBER(regexp_substr(A.SURVEYS.SURVEYID, '^\d+$')) in ({SURVEY_IDS})'''
     
+    MIN_SURVEY_ID = 20
 
-        # Class variable holding metadata type string
-    _metadata_type_id = 'Argus'
-    _filename_pattern = None  # Default RegEx for finding metadata file.
+    type_dict = {'g': 'GRAV',
+                 'm': 'MAG',
+                 'r': 'RAD'
+                }
+    
+    state_dict = {'V': 'VIC',
+                  'NS': 'NSW',
+                  'A': 'ACT',
+                  'Q': 'QLD',
+                  'NT': 'NT',
+                  'W': 'WA',
+                  'S': 'SA',
+                  'T': 'TAS'
+                 }
+    
+    def decode_state(self, state_tag):
+        return (ArgusMetadata.state_dict.get(state_tag[0]) or 
+                ArgusMetadata.state_dict.get(state_tag[0:2]) or 
+                state_tag)
 
     def __init__(self, db_user, db_password, db_alias, source=None):
         """Instantiates ArgusMetadata object. Overrides Metadata method
@@ -86,31 +108,76 @@ where TO_NUMBER(regexp_substr(A.SURVEYS.SURVEYID, '^\d+$')) = :SURVEYID
         self.connection = cx_Oracle.connect('%s/%s@%s' % (db_user, db_password, db_alias))
         self.cursor = self.connection.cursor()
         
-        self.cursor.prepare(ArgusMetadata.ARGUS_QUERY)
-
         if source:
-            try: # source is an integer survey_id
-                self.read_Argus_metadata(int(source))
-            except:
-                if isinstance(source, dict):
-                    self._metadata_dict = source
+            if isinstance(source, dict):
+                self._metadata_dict = source
+            else:
+                survey_ids = None
+                if type(source) == list: # list of survey_ids provided 
+                    survey_ids = source
+                else:
+                    try: # Try to convert comma separated list to list of integers
+                        survey_ids = [int(survey_id) for survey_id in self.list_from_string(source)]
+                    except:
+                        if type(source) == str:
+                            self.read_file(source) # Parse survey IDs from filename
+                        
+                if survey_ids:
+                    self.read_Argus_metadata(survey_ids)
 
-    def read_Argus_metadata(self, survey_id):
+                                     
+
+    def list_from_string(self, comma_separated_string):
+        '''
+        Helper function to return a list of strings from a string containing a comma separated list
+        '''
+        return [value_string.strip() for value_string in comma_separated_string.split(',') if value_string.strip()]
+    
+    def merge_metadata_dict(self, survey_metadata_dict):
+        '''
+        Helper function to merge new metadata dict into comma-separated lists in 
+        existing instance _metadata_dict. Need to avoid duplicates but preserve order.
+        '''
+        for key, values_string in survey_metadata_dict.iteritems():
+            values = self.list_from_string(values_string)
+            stored_values = self.list_from_string(self._metadata_dict.get(key) or '')
+
+            self._metadata_dict[key] = ', '.join(stored_values +
+                                                 [value for value in values if value not in stored_values]
+                                                 )
+            
+    def read_Argus_metadata(self, survey_ids):
         '''Read metadata from Argus query
         '''
-        query_result = self.cursor.execute(None, {'SURVEYID': survey_id})
+            
+        logger.info('Reading metadata from Argus query with survey IDs %s', survey_ids)
+
+        # N.B: Need to use text substitution for multiple survey IDs - can't use parameterised query
+        SQL = ArgusMetadata.ARGUS_QUERY.format(**{'SURVEY_IDS': 
+                                                  ', '.join([str(survey_id) 
+                                                             for survey_id in survey_ids
+                                                             ]
+                                                            )
+                                                  }
+                                               )
+        query_result = self.cursor.execute(SQL)
         
         if not self.argus_fields:
             self.argus_fields = [field_desc[0] for field_desc in query_result.description]
-            
-        argus_record = query_result.fetchone()
-        self._metadata_dict = dict(zip(self.argus_fields, [str(field) for field in argus_record]))
-        
-        assert not query_result.fetchone(), 'Multiple records returned for SURVEYID = %d' % survey_id
+                
+        for argus_record in query_result:
+            print argus_record
+            survey_metadata_dict = dict(zip(self.argus_fields, [str(field) if field else '' 
+                                                                for field in argus_record
+                                                                ]
+                                            )
+                                        )
+            self.merge_metadata_dict(survey_metadata_dict)
 
     def read_file(self, filename=None):
         '''
         Function to read Argus metadata from query and store the results in self._metadata_dict
+        using survey_id(s) parsed from filename
         Only included for compatibility with file-based metadata
         Argument:
             filename: Argus survey_id to query
@@ -119,14 +186,36 @@ where TO_NUMBER(regexp_substr(A.SURVEYS.SURVEYID, '^\d+$')) = :SURVEYID
 
         '''
         logger.debug('read_file(%s) called', filename)
+        
+        if not filename:
+            return
 
-        survey_id = int(filename) or int(self._filename)
-        assert survey_id, 'Survey ID must be specified'
+        basename = os.path.splitext(os.path.basename(filename))[0]
+        
+        match = re.match('([a-z])(\D+)', basename)
+        if not match:
+            return
+            
+        dataset_type = ArgusMetadata.type_dict.get(match.groups()[0].lower())
+        state = self.decode_state(match.groups()[1].upper())
+        
+        survey_ids = [survey_id for survey_id in [int(survey_id) 
+                                                  for survey_id in re.sub('\D+', ' ', basename).strip().split(' ')
+                                                  ]
+                      if survey_id > ArgusMetadata.MIN_SURVEY_ID
+                      ]
 
-        logger.debug('Reading metadata from Argus query with survey ID %d', survey_id)
-
-        self.read_Argus_metadata(survey_id)
+        self.read_Argus_metadata(survey_ids)
+        
         self._filename = filename
+        
+        argus_states = self.get_metadata('STATE')
+        if state not in argus_states:
+            logger.warning('Filename state "%s" inconsistend with Argus state "%s"' % (state, argus_states))
+
+        argus_dataset_types = self.get_metadata('DATATYPES')
+        if dataset_type not in argus_dataset_types:
+            logger.warning('Filename dataset type "%s" inconsistend with Argus dataset type "%s"' % (dataset_type, argus_dataset_types))
 
         return self._metadata_dict
 
---
+++ geophys2netcdf/metadata/_jetcat_metadata.py
@@ -0,0 +1,201 @@
+#!/usr/bin/env python
+
+"""JetCat Metadata module
+
+Author: Alex Ip (alex.ip@ga.gov.au)
+Written: 5/7/2016
+"""
+# TODO: Check potential issues with unicode vs str
+
+import logging
+from collections import OrderedDict
+import os
+import re
+import sys
+from pprint import pprint
+
+from geophys2netcdf.metadata import Metadata
+
+logger = logging.getLogger('root.' + __name__)
+logger.setLevel(logging.INFO)  # Initial logging level for this module
+
+class JetCatMetadata(Metadata):
+    """Subclass of Metadata to manage JetCat data
+    """
+    
+        # Class variable holding metadata type string
+    _metadata_type_id = 'JetCat'
+    _filename_pattern = '.*'  # Default RegEx for finding metadata file.
+    
+    JETCAT_PATH = '\\\\nas\\cdsm\\application\\GADDS\\JETCAT.dat' # Only works within GA
+
+    JETCAT_FIELDS = [
+        'NAME',
+        'LON1',   
+        'LAT1',   
+        'LON2',   
+        'LAT2',   
+        'LON3',   
+        'LAT3',   
+        'LON4',   
+        'LAT4',   
+        'CS',   
+        'THEME',   
+        'DATATYPE',   
+        'KSIZE',   
+        'DODS_URL',   
+        'LABEL',   
+        'DATE',   
+        'CELLSIZE',   
+        'FLAVOUR',   
+        'LINESPACING',   
+        'SURVEYID',   
+        'CSDATA',   
+        'LICENCENO',   
+        'CELLSIZE_M',   
+        ]
+
+    THEME_DICT = {'g': 'GRAVITY',
+                 'm': 'MAGNETICS',
+                 'r': 'RADIOMETRICS'
+                }
+    
+    STATE_DICT = {'V': 'VIC',
+                  'NS': 'NSW',
+                  'A': 'ACT',
+                  'Q': 'QLD',
+                  'NT': 'NT',
+                  'W': 'WA',
+                  'S': 'SA',
+                  'T': 'TAS'
+                 }
+    
+    def decode_state(self, state_tag):
+        return (JetCatMetadata.STATE_DICT.get(state_tag[0]) or 
+                JetCatMetadata.STATE_DICT.get(state_tag[0:2]) or 
+                state_tag)
+
+    def __init__(self, source=None):
+        """Instantiates JetCatMetadata object. Overrides Metadata method
+        """
+        self._metadata_dict = {}
+        
+        self.jetcat_fields = None
+
+        if source:
+            if isinstance(source, dict):
+                self._metadata_dict = source
+            elif type(source) == str:
+                self.read_file(source)
+
+                
+
+    def list_from_string(self, comma_separated_string):
+        '''
+        Helper function to return a list of strings from a string containing a comma separated list
+        '''
+        return [value_string.strip() for value_string in comma_separated_string.split(',') if value_string.strip()]
+    
+    def merge_metadata_dict(self, survey_metadata_dict):
+        '''
+        Helper function to merge new metadata dict into comma-separated lists in 
+        existing instance _metadata_dict. Need to avoid duplicates but preserve order.
+        '''
+        for key, values_string in survey_metadata_dict.iteritems():
+            values = self.list_from_string(values_string)
+            stored_values = self.list_from_string(self._metadata_dict.get(key) or '')
+
+            self._metadata_dict[key] = ', '.join(stored_values +
+                                                 [value for value in values if value not in stored_values]
+                                                 )
+            
+    def read_JetCat_metadata(self, survey_ids, theme=None, jetcat_path=None):
+        jetcat_path = jetcat_path or JetCatMetadata.JETCAT_PATH
+        
+        jetcat_file = open(jetcat_path, 'r')
+        self.jetcat_fields = None
+        for line in jetcat_file:
+            line = line.replace('\n', '')
+            values = [value.strip() for value in line.split('\t')]
+            
+            if self.jetcat_fields is None: # First line contains headers
+                self.jetcat_fields = [value.upper() for value in values] # Convert headers to upper case
+                assert self.jetcat_fields == JetCatMetadata.JETCAT_FIELDS, 'Invalid JetCat file format'
+                print self.jetcat_fields
+                continue
+            
+            jetcat_values = dict(zip(self.jetcat_fields, values))
+            
+            try:
+                survey_id = int(jetcat_values['SURVEYID'])
+            except:
+                survey_id = None
+            
+            if survey_id in survey_ids and  (not theme or theme in self.list_from_string(jetcat_values['THEME'])):
+                self.merge_metadata_dict(jetcat_values)
+    
+
+    def read_file(self, filename=None):
+        '''
+        Function to read JetCat metadata from query and store the results in self._metadata_dict
+        using survey_id(s) parsed from filename
+        Only included for compatibility with file-based metadata
+        Argument:
+            filename: Dataset filename to look up in jetcat file
+        Returns:
+            nested dict containing metadata
+
+        '''
+        logger.debug('read_file(%s) called', filename)
+        
+        if not filename:
+            return
+
+        basename = os.path.splitext(os.path.basename(filename))[0]
+        
+        match = re.match('([a-z])(\D+)', basename)
+        if not match:
+            return
+            
+        try:
+            theme = JetCatMetadata.THEME_DICT.get(match.groups()[0].lower())
+        except:
+            theme = None
+
+#        state = self.decode_state(match.groups()[1].upper())
+        
+        survey_ids = [int(survey_id) for survey_id in re.sub('\D+', ' ', basename).strip().split(' ')]
+
+        self.read_JetCat_metadata(survey_ids, theme)
+        
+        self._filename = filename
+        
+#===============================================================================
+#         jetCat_states = self.get_metadata('STATE')
+#         if state not in jetCat_states:
+#             logger.warning('Filename state "%s" inconsistent with JetCat state "%s"' % (state, jetCat_states))
+# 
+#         jetCat_dataset_types = self.get_metadata('DATATYPES')
+#         if dataset_type not in jetCat_dataset_types:
+#             logger.warning('Filename dataset type "%s" inconsistent with JetCat dataset type "%s"' % (dataset_type, jetCat_dataset_types))
+#===============================================================================
+
+        return self._metadata_dict
+
+    def write_file(self, filename=None, save_backup=False):
+        """Function write the metadata contained in self._metadata_dict to an JetCat file
+        Argument:
+            filename: Metadata file to be written
+        """
+        assert False, 'JetCat metadata is read-only'
+
+
+def main():
+    '''
+    Main function for quick and dirty testing
+    '''
+    jm = JetCatMetadata(*sys.argv[1:])
+    pprint(jm.metadata_dict)
+
+if __name__ == '__main__':
+    main()
