fbf84e084eafbcd4f80a533b32bc713d3830828a
Alex Ip <alex@trentham.net.au>
2017-02-10 15:03:32 +1100

    Bug fixed

--- utils/create_xml_metadata.py
+++ utils/create_xml_metadata.py
@@ -174,26 +174,8 @@ def main():
         nc_dataset.sync()
         print 'Fresh DOI %s generated and written to netCDF file' % dataset_uuid
         
-    distributions = []
-    dataset_doi = 'Calculated'
     if dataset_doi:
         calculated_values['DOI'] = str(dataset_doi) 
-          
-        distributions.append({'formatSpecification': 'html',
-                              'distributor_name': 'Geoscience Australia',
-                              'distributor_telephone': '+61 2 6249 9966',
-                              'distributor_address': 'GPO Box 378',
-                              'distributor_city': 'Canberra',
-                              'distributor_state': 'ACT',
-                              'distributor_postcode': '2601',
-                              'distributor_country': 'Australia',
-                              'distributor_email': 'clientservices@ga.gov.au',
-                              'url': dataset_doi,
-                              'protocol': 'WWW:LINK-1.0-http--link',
-                              'name': 'Digital Object Identifier',
-                              'description': 'Dataset DOI'
-                              }
-                             )
     
     WGS84_bbox = transform_coords(nc_grid_utils.native_bbox, nc_grid_utils.crs, 'EPSG:4326')
     WGS84_extents = [min([coordinate[0] for coordinate in WGS84_bbox]),
