8928cd1d8d75a8beae60a2d56f2f186b83c99bd3
Alex Ip <alex@trentham.net.au>
2017-03-09 18:02:43 +1100

    Logging fixed

--- utils/check_netcdf_vs_ers.py
+++ utils/check_netcdf_vs_ers.py
@@ -19,6 +19,14 @@ import numpy as np
 from collections import OrderedDict
 from functools import reduce
 
+# Set handler for root logger to standard output
+console_handler = logging.StreamHandler(sys.stdout)
+console_handler.setLevel(logging.INFO)
+# console_handler.setLevel(logging.DEBUG)
+console_formatter = logging.Formatter('%(message)s')
+console_handler.setFormatter(console_formatter)
+logging.root.addHandler(console_handler)
+
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)  # Initial logging level for this module
 
