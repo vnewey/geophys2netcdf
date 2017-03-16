bc4ce09e885061bdc8c5b7999c49b5a513425d79
Alex Ip <alex@trentham.net.au>
2017-03-16 16:12:50 +1100

    Different date formats in XML now supported

--- utils/create_xml_metadata.py
+++ utils/create_xml_metadata.py
@@ -79,6 +79,19 @@ def main():
                 print 'WARNING: Unable to create DOI distribution: %s' % e.message
         
         return xml_template.render(**value_dict)
+    
+    def str2datelist(multi_date_string):
+        '''
+        Helper function to convert comma-separated string containing dates to a list of dates
+        '''
+        date_list = []
+        for datetime_string in multi_date_string.split(','):
+            for format in ['%d-%b-%y', '%Y-%m-%dT%H:%M:%S']:
+                try:
+                    date_list.append(datetime.strptime(datetime_string.strip(), format).date())
+                except:
+                    continue
+        return date_list
 
     # Start of main function
     assert len(
@@ -157,20 +170,16 @@ def main():
     calculated_values['CELLSIZE_M'] = str(int(round((nc_grid_utils.nominal_pixel_metres[0] + nc_grid_utils.nominal_pixel_metres[1]) / 20.0) * 10))
     calculated_values['CELLSIZE_DEG'] = str(round((nc_grid_utils.nominal_pixel_degrees[0] + nc_grid_utils.nominal_pixel_degrees[1]) / 2.0, 8))
     
+            
+                                                                          
     try:
-        calculated_values['START_DATE'] = min([datetime.strptime(datetime_string.strip(), '%d-%b-%y').date() 
-                                               for datetime_string in str(metadata_object.get_metadata(['Survey', 'STARTDATE'])
-                                                                          ).split(',')]
-                                              ).isoformat()
-    except:
+        calculated_values['START_DATE'] = min(str2datelist(str(metadata_object.get_metadata(['Survey', 'STARTDATE'])))).isoformat()
+    except ValueError:
         calculated_values['START_DATE'] = None   
     
     try:
-        calculated_values['END_DATE'] = max([datetime.strptime(datetime_string.strip(), '%d-%b-%y').date() 
-                                               for datetime_string in str(metadata_object.get_metadata(['Survey', 'ENDDATE'])
-                                                                          ).split(',')]
-                                              ).isoformat()
-    except:
+        calculated_values['END_DATE'] = max(str2datelist(str(metadata_object.get_metadata(['Survey', 'ENDDATE'])))).isoformat()
+    except ValueError:
         calculated_values['END_DATE'] = None 
     
     #history = "Wed Oct 26 14:34:42 2016: GDAL CreateCopy( /local/el8/axi547/tmp/mWA0769_770_772_773.nc, ... )"
