8bca8663f4678ff3e2692074d6b9f88f0719c8b1
Alex Ip <alex@trentham.net.au>
2017-04-07 10:38:15 +1000

    Detection method for directories vs files improved

--- geophys2netcdf/thredds_catalog/__main__.py
+++ geophys2netcdf/thredds_catalog/__main__.py
@@ -8,7 +8,18 @@ import sys
 import os
 import re
 #from pprint import pprint
+import logging
 from geophys2netcdf.thredds_catalog import THREDDSCatalog
+
+# Set handler for root logger to standard output
+#console_handler = logging.StreamHandler(sys.stdout)
+#console_handler.setLevel(logging.DEBUG)
+#console_formatter = logging.Formatter('%(message)s')
+#console_handler.setFormatter(console_formatter)
+
+logger = logging.getLogger(__name__)
+#logger.addHandler(console_handler)
+logger.setLevel(logging.DEBUG)  # Logging level for this module
 
 def main():
     assert len(sys.argv) == 2, 'Usage: %s <thredds_catalog_url>|<yaml_file_path>' % sys.argv[0]
